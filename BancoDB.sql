7a7341840443962f57b989f25c7514b1afa23f70
Gabriel Fellipe Marquardt <119713462+DevMarquardt@users.noreply.github.com>
2023-10-10 21:59:45 -0300

    Banco de dados utilizado para a atividade

---
+++ BancoDB.sql
@@ -0,0 +1,82 @@
+CREATE DATABASE  IF NOT EXISTS `banco` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `banco`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: banco
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `conta_corrente`
+--
+
+DROP TABLE IF EXISTS `conta_corrente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `conta_corrente` (
+  `id_conta_corrente` int NOT NULL,
+  `limite` double NOT NULL,
+  `total_de_transacoes` int NOT NULL,
+  PRIMARY KEY (`id_conta_corrente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conta_corrente`
+--
+
+LOCK TABLES `conta_corrente` WRITE;
+/*!40000 ALTER TABLE `conta_corrente` DISABLE KEYS */;
+INSERT INTO `conta_corrente` VALUES (1,1000,1),(3,3,3),(5,10,10);
+/*!40000 ALTER TABLE `conta_corrente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contas`
+--
+
+DROP TABLE IF EXISTS `contas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contas` (
+  `id_conta` int NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `saldo` double NOT NULL,
+  `id_conta_corrente` int DEFAULT NULL,
+  PRIMARY KEY (`id_conta`),
+  KEY `id_conta_corrente` (`id_conta_corrente`),
+  CONSTRAINT `contas_ibfk_1` FOREIGN KEY (`id_conta_corrente`) REFERENCES `conta_corrente` (`id_conta_corrente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contas`
+--
+
+LOCK TABLES `contas` WRITE;
+/*!40000 ALTER TABLE `contas` DISABLE KEYS */;
+INSERT INTO `contas` VALUES (1,'Usuario Admin',2000,1),(2,'Usuario teste',1,NULL),(3,'Usuario teste2',1,5);
+/*!40000 ALTER TABLE `contas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-10 21:59:05
